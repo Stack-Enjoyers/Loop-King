2c9a9eaf3978445bcf0a2ea6afb26019d686868e
umutucak <umutucak@proton.me>
2024-06-14 15:27:14 +0200

    lane switching fully implemented with map boundaries

--- scripts/car.gd
+++ scripts/car.gd
@@ -5,14 +5,17 @@ extends CharacterBody2D
 
 var speed = 200
 var lane_switch_distance = null
-func _process(delta):
-	lane_switch_distance = collision_shape2d.shape.size[1]
-	# lane switching controls
-	var direction = Input.get_axis("car_up", "car_down")
-	if direction:
-		lane_switch(direction)
+
+func _ready():
+	lane_switch_distance = collision_shape2d.shape.get_rect().size[1]
+
+func _input(event):
+	if event.is_action_pressed("car_up") and character_body2d.position.y > -73.75:
+		lane_switch(-1)
+	elif event.is_action_pressed("car_down") and character_body2d.position.y < 73.75:
+		lane_switch(1)
 
 func lane_switch(direction):
 	var previous_location = character_body2d.position
-	var target_location = previous_location.y + (direction * lane_switch_distance)
+	var target_location = previous_location.y + (direction * lane_switch_distance * 2.5)
 	character_body2d.position = Vector2(previous_location.x, target_location)
