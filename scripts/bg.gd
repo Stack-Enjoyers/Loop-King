f0c32eedb8bfbfcc7eb26aacfb477bc91c5c2081
raigeki49 <calvo.dorian14@gmail.com>
2024-06-16 01:17:20 +0200

    the more speed the faster background moves

--- scripts/bg.gd
+++ scripts/bg.gd
@@ -2,6 +2,8 @@ extends ParallaxBackground
 @onready var bg = $"."
 
 var scroll = 0
+var standard_speed = 100
+var current_speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,5 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll -= 100 * delta
+	scroll -= current_speed * delta
 	bg.scroll_offset.x = scroll
+
+
+func _on_combo_screen_speed_changed(new_speed):
+	print("hello")
+	current_speed = standard_speed + new_speed
